528bdb7ed3c51ce9f7a724bac9ef818a10371d95
billwurles <burlesw@gmail.com>
2017-09-12 18:03:08 +0100

    build upload

---
+++ app/build/generated/source/buildConfig/debug/willburles/brookes/ac/musicalsensors/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package willburles.brookes.ac.musicalsensors;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "willburles.brookes.ac.musicalsensors";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
